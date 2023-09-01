cc57e43b46435aae3b48ed6ca0fdc72845217c9d
rafaneo <rafailmaximos@hotmail.com>
2023-09-01 16:08:09 +0300

    added passwords

--- index2.js
+++ index2.js
@@ -1,4 +1,4 @@
-passwords_list = [1111,2222,3333];
+passwords_list = [1452,3329,7186];
 
 document.getElementById("login_2").addEventListener("click", function(){
     if(passwords_list.includes(parseInt(document.getElementById("password_2").value))){
